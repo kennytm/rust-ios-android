fd434b53096b18bc5322085ab20a61c9def6396a
kennytm <kennytm@gmail.com>
2016-07-24 00:48:31 +0800

    Update scripts.

--- package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -euo pipefail
-
-TODAY=$(date -ju "+%Y-%m-%d")
-VERSION=${1-nightly}
-
-tar cfvJ "rust-ios-android-${VERSION}-${TODAY}.tar.xz" "build-${VERSION}/x86_64-apple-darwin/stage2"
-
--- sample/build.sh
+++ sample/build.sh
@@ -3,19 +3,19 @@
 set -euo pipefail
 
 # Build the rust project.
-
 cd cargo
-../../cargo-all-targets.py clean
-../../cargo-all-targets.py build-lib --release
+cargo clean
+cargo test
 
-# Convert to iOS library
-../../ios-fat-lib.sh release libsample
+cargo lipo --release
+cargo build --target aarch64-linux-android --release
+cargo build --target armv7-linux-androideabi --release
+cargo build --target i686-linux-android --release
 
+# Build the Android project.
 cd ../android
-
 gradle assembleRelease
 
+# Build the iOS project.
 cd ../ios
-
 xcodebuild
-
